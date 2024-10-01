a2d8ec3f720ba555b3f1323b63eff014d5417a12
coconhat <nhat_vu_le@dlsl.edu.ph>
2024-10-02 00:19:30 +0800

    add display all items and update items

--- inventory.cpp
+++ inventory.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    
+class Inventory
+{
+
+// constructor
+Inventory(){}
+
+//add item function
+void addItem(){}
+
+// 
+};
+
+class Item{
+private: 
+vector
+};
+
+int main()
+{
+
+    {
+        ("P001", "Black and Gray Athletic Cotton Socks - 6 Pairs", 26, 180),
+            ("P002", "Intermediate Size Basketball", 3, 1999),
+            ("P003", "Adults Plain Cotton T-Shirt - 2 Pack", 400, 799),
+            ("P004", "2 Slot Toaster - Black", 120, 3000),
+            ("P005", "6 Piece White Dinner Plate Set", 123, 1499),
+            ("P006", "6-Piece Nonstick, Carbon Steel Oven Bakeware Set", 4, 499),
+            ("P007", "Plain Hooded Fleece Sweatshirt", 83, 1199),
+            ("P008", "Luxury Towel Set - Graphite Gray", 2, 999),
+            ("P009", "Liquid Laundry Detergent, 110 Loads", 6, 799),
+            ("P010", "Waterproof Knit Athletic Sneakers - Gray", 5, 2999),
+            ("P011", "Women's Chiffon Beachwear Cover Up - Black", 32, 699);
+    };
 }
